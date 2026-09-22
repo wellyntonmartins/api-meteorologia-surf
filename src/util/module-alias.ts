f6888cdca0e7e719a4081ce70ac61af489a2d119
Wellynton João Martins <wellyntonjoaomartins@gmail.com>
2026-09-21 22:41:49 -0300

    chore:fix resolvido problema de versionamento do code

--- src/util/module-alias.ts
+++ src/util/module-alias.ts
@@ -1,5 +1,5 @@
 import * as path from "path";
-import moduleAlias, { addAlias, addAliases } from "module-alias";
+import moduleAlias, { addAliases } from "module-alias";
 
 const files = path.resolve(__dirname, "../..");
 
